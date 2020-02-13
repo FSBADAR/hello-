f63c0b156b5f3324feaa7287edc5629e80f7252d
fawwaaz badar <bfawwaaz@gmail.com>
2020-02-12 19:02:00 -0500

    hello c++

---
+++ hello.cpp
@@ -0,0 +1,14 @@
+// my first program in C++
+#include <iostream>
+using namespace std;
+
+
+int main()
+{
+	std::cout << "Hello World! ";
+	std::cout 
+	  << "I'm a C++ program";
+	std::cout << "Hello World! ";
+	std::cout << "Hello World! ";
+std::cout << "Hello World! ";
+}
